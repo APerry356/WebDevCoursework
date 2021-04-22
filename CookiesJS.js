7cc8165ee3ed810e4b056f5130be3d22d94ea7fc
APerry356 <ahht356@gmail.com>
2021-04-22 18:33:57 +0100

    comments Update

--- CookiesJS.js
+++ CookiesJS.js
@@ -1,17 +1,13 @@
-function setScoreCookie() {
-	document.cookie = "score=20";
-}
-
 // Functions for charecter name got from W3 Schools (https://www.w3schools.com/js/js_cookies.asp)
 
-function setCookie(cname, cvalue, exdays) {
+function setCookie(cname, cvalue, exdays) { // function to set a cookie
   var d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
   var expires = "expires="+ d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-function getCookie(cname) {
+function getCookie(cname) { // function to get a cookie for use by another function
   var name = cname + "=";
   var decodedCookie = decodeURIComponent(document.cookie);
   var ca = decodedCookie.split(';');
@@ -27,19 +23,12 @@ function getCookie(cname) {
   return "";
 }
 
-function checkCookie() {
-  var username = getCookie("username");
-  if (username != "") {
-   alert("Welcome again " + username);
-  } else {
-    username = prompt("Please enter your Charecters name:", "");
-    if (username != "" && username != null) {
-      setCookie("username", username, 30);
-    }
-  }
+function checkCookie() { // function for getting the charecter name
+	username = prompt("Please enter your Charecters name:", "");
+    setCookie("username", username, 30);
 }
 
-function getUsername() {
+function getUsername() { // function to show username with score on end page
 	username = getCookie("username");
 	score = getCookie("score")
 	alert ("You have completed your Journey " + username + ", but what were the consiquences of your actions? Your morality Score was: " + score);
@@ -53,6 +42,8 @@ function archerChoice(choiceno, choiceVal) {
     setCookie(choiceno, choiceVal, 365);
 }
 
+
+// Functions for Score function.
 function neutralDec() {
 	var sCookie = getCookie(score);
 	score = sCookie - 0
@@ -76,6 +67,9 @@ function endKnight() {
 	getCookie(choice1);	
 }
 
+
+
+// Functions for saving which option a user picked in each scenario 
 function makeChoice1() {
     var kchoice1 = getCookie("choice1");
     console.log(kchoice1);
@@ -113,7 +107,7 @@ function makeChoice3() {
 }
 	
 	
-function makeChoice() {
+function makeChoice() { // function to run all required functions for end screen.
 	getUsername();
 	makeChoice1();
 	makeChoice2();
